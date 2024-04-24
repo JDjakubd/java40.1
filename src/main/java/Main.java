ca4db7780e88607f63f6a1cbd9d6aa2d96f4dc2f
JDjakubd <162459443+JDjakubd@users.noreply.github.com>
2024-04-24 13:11:24 +0000

    p1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
@@ -16,9 +17,41 @@ class Main {
       s.addStudent(new Student("Krzysztof", 20));
       s.addStudent(new Student("Janusz", 40));
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+      Scanner scanner = new Scanner(System.in);
+      boolean choice = true;
+      while (choice) {
+        System.out.println("Menu:");
+        System.out.println("1. Dodaj nowego studenta");
+        System.out.println("2. Wypisz wszystkich studentów");
+        System.out.println("3. Wyjście");
+        System.out.print("Wybierz opcję: ");
+
+        int choice2 = scanner.nextInt();
+        scanner.nextLine();
+        switch (choice2) {
+          case 1:
+            System.out.println("Dodaj nowego studenta:");
+            System.out.print("Imię: ");
+            String name = scanner.next();
+            System.out.print("Wiek: ");
+            int age = scanner.nextInt();
+            scanner.nextLine();
+            s.addStudent(new Student(name, age));
+            break;
+          case 2:
+            System.out.println("Lista studentów:");
+            var students = s.getStudents();
+            for (Student current : students) {
+              System.out.println(current.ToString());
+            }
+            break;
+          case 3:
+            choice = false;
+            break;
+          default:
+            System.out.println("Niepoprawny wybór. Spróbuj ponownie.");
+            break;
+        }
       }
     } catch (IOException e) {
 
